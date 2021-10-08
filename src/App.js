d0e9b523ee824d742ffbb1cbedbdd36efd1a4786
ansarkhalil <ansarkhalil93@gmail.com>
2021-10-08 19:17:42 +0500

    login methods

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Login from './Login'
 import './App.css';
 
 function App() {
     return (
         <div className="App">
-
+            <Router>
+                <Switch>
+                    <Route exact path="/" component={Login}/>
+                </Switch>
+            </Router>
         </div>
     );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
-import firebase from "firebase";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth"
+import "firebase/compat/firestore"
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -39,4 +41,53 @@ const signInWithGoogle = async () => {
         console.error(err);
         alert(err.message);
     }
+};
+
+const signInWithEmailAndPassword = async (email, password) => {
+    try {
+        await auth.signInWithEmailAndPassword(email, password);
+    } catch (err) {
+        console.error(err);
+        alert(err.message);
+    }
+};
+
+const registerWithEmailAndPassword = async (name, email, password) => {
+    try {
+        const res = await auth.createUserWithEmailAndPassword(email, password);
+        const user = res.user;
+        await db.collection("users").add({
+            uid: user.uid,
+            name,
+            authProvider: "local",
+            email,
+        });
+    } catch (err) {
+        console.error(err);
+        alert(err.message);
+    }
+};
+
+const sendPasswordResetEmail = async (email) => {
+    try {
+        await auth.sendPasswordResetEmail(email);
+        alert("Password reset link sent!");
+    } catch (err) {
+        console.error(err);
+        alert(err.message);
+    }
+};
+
+const logout = () => {
+    auth.signOut();
+};
+
+export {
+    auth,
+    db,
+    signInWithGoogle,
+    signInWithEmailAndPassword,
+    registerWithEmailAndPassword,
+    sendPasswordResetEmail,
+    logout,
 };
